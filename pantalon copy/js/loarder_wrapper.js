d243c2ee6d181297a7905b8800a5f323c63e7021
AbdoHerO <abdo.sbeta.12@gmail.com>
2023-12-16 21:52:12 +0100

    changes

--- pantalon copy/js/loarder_wrapper.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("load", function () {
-  $(".loader").hide();
-  $("body").removeClass("body_fixed");
-});
-
-// document.addEventListener("DOMContentLoaded", function () {
-//     var loaderWrapper = document.getElementById("loader-wrapper");
-//     loaderWrapper.style.display = "none";
-//   });
