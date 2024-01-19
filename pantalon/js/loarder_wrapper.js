99fd39e11818ab1b98207ecc85b50cd613545e9e
alaeddine58 <49734355+alaeddine58@users.noreply.github.com>
2024-01-19 23:27:13 +0100

    Update index.html

--- pantalon/js/loarder_wrapper.js
+++ pantalon/js/loarder_wrapper.js
@@ -1,9 +1,17 @@
-window.addEventListener("load", function () {
-  $(".loader").hide();
-  $("body").removeClass("body_fixed");
-});
+// window.addEventListener("load", function () {
+//   $(".loader").hide();
+//   $("body").removeClass("body_fixed");
+// });
 
 // document.addEventListener("DOMContentLoaded", function () {
 //     var loaderWrapper = document.getElementById("loader-wrapper");
 //     loaderWrapper.style.display = "none";
 //   });
+
+// setTimeout(function () {
+//   $(".loader").hide();
+//   $("body").removeClass("body_fixed");
+// }, 100); // 500 milliseconds (0.5 seconds) delay
+
+$(".loader").hide();
+$("body").removeClass("body_fixed");
