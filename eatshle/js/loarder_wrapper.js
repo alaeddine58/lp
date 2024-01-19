10a225f60f8cca0d40a7803f1e4a1471d29fc1b4
AbdoHerO <abdo.sbeta.12@gmail.com>
2024-01-19 21:29:26 +0100

    add casual-pant-decontracte lp

--- eatshle/js/loarder_wrapper.js
+++ eatshle/js/loarder_wrapper.js
@@ -11,4 +11,4 @@
 setTimeout(function () {
   $(".loader").hide();
   $("body").removeClass("body_fixed");
-}, 500); // 500 milliseconds (0.5 seconds) delay
+}, 100); // 500 milliseconds (0.5 seconds) delay
