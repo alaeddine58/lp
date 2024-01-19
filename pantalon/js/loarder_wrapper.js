4882ecf4c040574cbc545779fa6480d0028c2509
AbdoHerO <abdo.sbeta.12@gmail.com>
2024-01-19 20:56:08 +0100

    add casual-pant-decontracte lp

--- pantalon/js/loarder_wrapper.js
+++ pantalon/js/loarder_wrapper.js
@@ -1,9 +1,14 @@
-window.addEventListener("load", function () {
-  $(".loader").hide();
-  $("body").removeClass("body_fixed");
-});
+// window.addEventListener("load", function () {
+//   $(".loader").hide();
+//   $("body").removeClass("body_fixed");
+// });
 
 // document.addEventListener("DOMContentLoaded", function () {
 //     var loaderWrapper = document.getElementById("loader-wrapper");
 //     loaderWrapper.style.display = "none";
 //   });
+
+setTimeout(function () {
+  $(".loader").hide();
+  $("body").removeClass("body_fixed");
+}, 500); // 500 milliseconds (0.5 seconds) delay
