8db222a16f3590b07d3d4ee4b17b194906341909
alaeddine58 <49734355+alaeddine58@users.noreply.github.com>
2024-12-05 22:27:21 +0100

    Update api_cozmo_landing_page.js

--- pantalon-offre-2eme-gratuit/api_cozmo_landing_page.js
+++ pantalon-offre-2eme-gratuit/api_cozmo_landing_page.js
@@ -92,7 +92,7 @@ $("#formInfo").submit(function (event) {
   console.log("sheetDBData", sheetDBData);
 
   // Insert into SheetDB API
-  fetch("https://sheetdb.io/api/v1/ei6ndxolykn31", {
+  fetch("https://sheetdb.io/api/v1/9ck3owwv3wurm", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
