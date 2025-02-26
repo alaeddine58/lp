d8b83d4ae717f6eef7ba3793e0b22d9c9210e060
alaeddine58 <49734355+alaeddine58@users.noreply.github.com>
2025-02-26 11:57:44 +0000

    Bt Price adjust

--- baggy-trousers-destockage/api2.js
+++ baggy-trousers-destockage/api2.js
@@ -97,16 +97,16 @@ document.addEventListener("DOMContentLoaded", function () {
             const packHeader = packDetailsVisible.previousElementSibling.querySelector('.title').textContent;
 
             switch (packHeader) {
-                case 'واحد ب 199 درهم':
-                    total = 200;
+                case 'واحد ب 189 درهم':
+                    total = 190;
                     quantity = 1;
                     break;
                 case 'إثنان ب 299 فقط':
                     total = 300;
                     quantity = 2;
                     break;
-                case '2 ب 399 درهم + الثالث مجانا':
-                    total = 400;
+                case '2 ب 349 درهم + الثالث مجانا':
+                    total = 350;
                     quantity = 3;
                     break;
                 default:
